165431543ce6e13644dec2fe005766c4121c61ee
Ku6epnec <ku6epnec@rambler.ru>
2021-07-09 12:31:30 +0300

    Итог по курсу 2DCarGame

---
+++ Assets/Scripts/Bundles/AssetBundleBase.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AssetBundleBase : MonoBehaviour
+{
+    private const string UrlAssetBundleSprite = "https://github.com/Ku6epnec/Bundles/raw/master/LZMA/menubundle";
+    private const string UrlAssetBundleAudio = "https://github.com/Ku6epnec/Bundles/raw/master/LZMA/audiobundle";
+
+    [SerializeField]
+    private DataSpriteBundle[] _dataSpriteBundles;
+
+    [SerializeField]
+    private DataAudioBundle[] _dataAudioBundles;
+
+    private AssetBundle _spriteAssetBundle;
+    private AssetBundle _audioAssetBundle;
+
+    protected IEnumerator DownloadAndSetAssetBundle()
+    {
+        yield return GetSpriteAssetBundle();
+        yield return GetAudioAssetBundle();
+
+        if (_spriteAssetBundle == null || _audioAssetBundle == null)
+        {
+            Debug.LogError("Error Download");
+            yield break;
+        }
+
+        SetAssetBundle();
+        yield return null;
+    }
+
+    private IEnumerator GetSpriteAssetBundle()
+    {
+        var request = UnityWebRequestAssetBundle.GetAssetBundle(UrlAssetBundleSprite);
+        yield return request.SendWebRequest();
+
+        while (!request.isDone)
+            yield return null;
+
+        GetContentAssetBundle(request, ref _spriteAssetBundle);
+    }
+
+    private IEnumerator GetAudioAssetBundle()
+    {
+        var request = UnityWebRequestAssetBundle.GetAssetBundle(UrlAssetBundleAudio);
+        yield return request.SendWebRequest();
+
+        while (!request.isDone)
+            yield return null;
+
+        GetContentAssetBundle(request, ref _audioAssetBundle);
+    }
+
+    private void GetContentAssetBundle(UnityWebRequest request, ref AssetBundle assetBundle)
+    {
+        if (request.error == null)
+        {
+            assetBundle = DownloadHandlerAssetBundle.GetContent(request);
+            Debug.Log("Download mission Completed!");
+        }
+
+        else
+        {
+            Debug.LogError(request.error);
+        } 
+            
+    }
+
+    private void SetAssetBundle()
+    {
+        foreach (var data in _dataSpriteBundles)
+            data.Image.sprite = _spriteAssetBundle.LoadAsset<Sprite>(data.NameAssetBundle);
+
+        foreach (var data in _dataAudioBundles)
+        {
+            data.AudioSource.clip = _audioAssetBundle.LoadAsset<AudioClip>(data.NameAssetBundle);
+            data.AudioSource.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu/MainMenuView.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Ui
+{
+    public class MainMenuView : AssetBundleBase
+    {
+        [SerializeField] 
+        private Button _buttonStart;
+
+        [SerializeField]
+        private Button _buttonDailyReward;
+
+        [SerializeField]
+        private Button _buttonExit;
+
+        [SerializeField]
+        private Button _loadAssetBundle;
+
+        private void Start()
+        {
+            _loadAssetBundle.onClick.AddListener(LoadAsset);
+        }
+
+        public void Init(UnityAction startGame, UnityAction watchDailyReward)
+        {
+            _buttonStart.onClick.AddListener(startGame);
+            _buttonDailyReward.onClick.AddListener(watchDailyReward);
+            _buttonExit.onClick.AddListener(Exit);
+        }
+
+        protected void OnDestroy()
+        {
+            _buttonStart.onClick.RemoveAllListeners();
+            _buttonExit.onClick.RemoveAllListeners();
+            _buttonDailyReward.onClick.RemoveAllListeners();
+            _loadAssetBundle.onClick.RemoveAllListeners();
+        }
+
+        private void LoadAsset()
+        {
+            _loadAssetBundle.interactable = false;
+
+            StartCoroutine(DownloadAndSetAssetBundle());
+        }
+
+        private void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
+
