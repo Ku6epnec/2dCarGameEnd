a302f5c1c7ea0e2262b386f7e70d96d0937f9c92
Ku6epnec <ku6epnec@rambler.ru>
2021-07-09 14:31:19 +0300

    Предпоследний вариант

---
+++ Assets/Scripts/MainMenu/Localization.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
+
+public class Localization : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text _rewardText;
+    [SerializeField]
+    private TMP_Text _startText;
+    [SerializeField]
+    private TMP_Text _exitText;
+    [SerializeField]
+    private TMP_Text _russianText;
+    [SerializeField]
+    private TMP_Text _englishText;
+    [SerializeField]
+    private TMP_Text _bundleText;
+    [SerializeField]
+    private TMP_Text _notificationText;
+    [SerializeField]
+    private TMP_Text _prefabText;
+
+    [SerializeField]
+    private Button _russianButton;
+
+    [SerializeField]
+    private Button _englishButton;
+
+
+    private void Start()
+    {
+        ChangedLocaleEvent(null);
+        LocalizationSettings.SelectedLocaleChanged += ChangedLocaleEvent;
+
+        _russianButton.onClick.AddListener(() => ChangeLanguage(1));
+        _englishButton.onClick.AddListener(() => ChangeLanguage(0));
+    }
+
+    private void OnDestroy()
+    {
+        _russianButton.onClick.RemoveAllListeners();
+        _englishButton.onClick.RemoveAllListeners();
+    }
+
+    private void ChangedLocaleEvent(Locale locale)
+    {
+        StartCoroutine(OnChangedLocale(locale));
+    }
+
+    private IEnumerator OnChangedLocale(Locale locale)
+    {
+        var loadingOperation = LocalizationSettings.StringDatabase.GetTableAsync("Language");
+        yield return loadingOperation;
+
+        if (loadingOperation.Status == AsyncOperationStatus.Succeeded)
+        {
+            var table = loadingOperation.Result;
+            _rewardText.text = table.GetEntry("reward_menu")?.GetLocalizedString();
+            _startText.text = table.GetEntry("start")?.GetLocalizedString();
+            _exitText.text = table.GetEntry("exit")?.GetLocalizedString();
+            _russianText.text = table.GetEntry("russian")?.GetLocalizedString();
+            _englishText.text = table.GetEntry("english")?.GetLocalizedString();
+            _bundleText.text = table.GetEntry("load_bundle")?.GetLocalizedString();
+            _notificationText.text = table.GetEntry("notification")?.GetLocalizedString();
+            _prefabText.text = table.GetEntry("load_prefab")?.GetLocalizedString();
+
+        }
+        else
+        {
+            Debug.Log("Could not load String Table\n" + loadingOperation.OperationException);
+        }
+    }
+
+    private void ChangeLanguage(int index)
+    {
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+    }
+}
---
+++ Assets/Scripts/Notification/NotificationWindow.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Unity.Notifications.Android;
+using Unity.Notifications.iOS;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationWindow : MonoBehaviour
+{
+    private const string AndroidNotifierId = "android_notifier_id";
+
+    [SerializeField]
+    private Button _buttonNotification;
+
+    private void Start()
+    {
+        _buttonNotification.onClick.AddListener(CreateNotification);
+    }
+
+    private void OnDestroy()
+    {
+        _buttonNotification.onClick.RemoveAllListeners();
+    }
+
+    private void CreateNotification()
+    {
+#if UNITY_ANDROID
+        var androidSettingsChanel = new AndroidNotificationChannel
+        {
+            Id = AndroidNotifierId,
+            Name = "Game Notifier",
+            Importance = Importance.High,
+            CanBypassDnd = true,
+            CanShowBadge = true,
+            Description = "Enter the best game ever, we miss you!",
+            EnableLights = true,
+            EnableVibration = true,
+            LockScreenVisibility = LockScreenVisibility.Public
+        };
+
+        AndroidNotificationCenter.RegisterNotificationChannel(androidSettingsChanel);
+
+        var androidSettingsNotification = new AndroidNotification
+        {
+            Color = Color.blue,
+            RepeatInterval = TimeSpan.FromSeconds(10)
+        };
+
+        AndroidNotificationCenter.SendNotification(androidSettingsNotification, AndroidNotifierId);
+#elif UNITY_IOS
+       var iosSettingsNotification = new iOSNotification
+       {
+           Identifier = "android_notifier_id",
+           Title = "Game Notifier",
+           Subtitle = "Subtitle notifier",
+           Body = "Enter the best game ever, we miss you!",
+           Badge = 1,
+           Data = "01/02/2021",
+           ForegroundPresentationOption = PresentationOption.Alert,
+           ShowInForeground = false
+       };
+      
+       iOSNotificationCenter.ScheduleNotification(iosSettingsNotification);
+#endif
+    }
+}
